b001efb8c03959079338704280d4e9a5f2ea915c
Nickolay Malysh <stalcrot@yandex.ru>
2020-01-11 17:47:36 +0300

    настроил отображение постов в админке

--- yatube/posts/admin.py
+++ yatube/posts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отобрадаться в админке
+    list_display = ('pk', 'text', 'pub_date', 'author')
+    # интерфейс для поиска по тексту постов
+    search_fields = ('text',)
+    # добавляем возможность фильтрации по дате
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'  # это свойство сработает для всех колонок: где пусто - там будет эта строка
+
+
+# при регистрации модели Post, источником информации для неё назначен PostAdmin
+admin.site.register(Post, PostAdmin)
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Post
 
-# Create your views here.
+
+def index(request):
+    # получаем результат из нашей БД
+    latest = Post.objects.order_by('-pub_date')[:10]
+    # собираем тексты постов в один, разделяя новой строкой
+    output = list()
+    for item in latest:
+        output.append(item.text)
+    return HttpResponse('\n'.join(output))
