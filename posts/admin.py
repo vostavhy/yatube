015e2e8ecc423566c4041486510a77a684a8090f
Nickolay Malysh <stalcrot@yandex.ru>
2020-01-28 21:07:54 +0300

    change project structure

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Group
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -14,3 +14,4 @@ class PostAdmin(admin.ModelAdmin):
 
 # при регистрации модели Post, источником информации для неё назначен PostAdmin
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group)
