45712ce17568e3ce8248fc139cb30c1ef0e6fb7b
Nickolay Malysh <stalcrot@yandex.ru>
2020-01-28 23:03:10 +0300

    add new app users

--- yatube/urls.py
+++ yatube/urls.py
@@ -17,7 +17,16 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    # раздел администратора
     path('admin/', admin.site.urls),
-    # импорт правил из приложения posts
+
+    # обработчик главной страницы ищем в urls.py приложения posts
     path('', include('posts.urls')),
+
+    # регистрация и авторизация
+    path('auth', include('users.urls')),
+
+    # если нужного шаблона для /auth не нашлось в файле users.urls -
+    # ищем совпадения в файле "django.conrib.auth.urls"
+    path('auth', include('users.urls')),
 ]
