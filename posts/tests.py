189264d379246d3cd80f63a909a83997fbd7ed32
Nickolay Malysh <stalcrot@yandex.ru>
2020-01-30 19:11:58 +0300

    добавил форму регистрации

--- posts/tests.py
+++ posts/tests.py
@@ -1,4 +1,12 @@
 from django.test import TestCase
 import datetime as dt
 
-print(dt.datetime.now().year)
+# print(dt.datetime.now().year)
+my_dict = {
+    'first_key': 'first_value',
+    'second_key': 'second_value',
+    }
+
+for i in my_dict:
+    print(i)
+
