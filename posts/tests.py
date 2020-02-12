a7ed99957b7f448b26811f6ced040721910bd875
Nickolay Malysh <stalcrot@yandex.ru>
2020-02-12 15:59:03 +0300

    add testcase

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 
+class TestStringMethods(TestCase):
+    def test_length(self):
+        self.assertEqual(len('yatube'), 6)
+
+    def test_show_msg(self):
+        self.assertTrue(False, msg='Важная проверка на инстинность')
