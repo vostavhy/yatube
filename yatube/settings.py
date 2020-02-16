d3beeb2ae6bc7a2b8ee6303312fddf605793350f
Nickolay Malysh <stalcrot@yandex.ru>
2020-02-16 15:32:31 +0300

    добавил flatpages

--- yatube/settings.py
+++ yatube/settings.py
@@ -38,6 +38,8 @@ ALLOWED_HOSTS = [
 INSTALLED_APPS = [
     'users',
     'posts',
+    'django.contrib.sites',
+    'django.contrib.flatpages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -143,3 +145,7 @@ LOGIN_REDIRECT_URL = "index"
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 # дирректория, куда будут складываться файлы писем
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+
+# Идентификатор текущего сайта
+SITE_ID = 1
+
